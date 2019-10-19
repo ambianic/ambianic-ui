1cef31097c435ee038b74e46f1f9e938b4e0d7a6
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-10-19 01:36:15 -0500

    fix: upgrade to vuetify 2

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     'workboxOptions': {
       'runtimeCaching': [
         {
-          'urlPattern': {},
+          'urlPattern': new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
           'handler': 'CacheFirst',
           'options': {
             'cacheName': 'google-fonts',
