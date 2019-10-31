ef8975c4c2103f795f5e6cac1783ccda7d76b5f4
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-10-30 21:21:57 -0500

    feat: introduce sample timeline

--- src/App.vue
+++ src/App.vue
@@ -136,103 +136,13 @@
     <v-content>
       <v-container
         id="container"
-        class="fill-height"
+        class="pa-0 ma-0"
         fluid
       >
         <router-view></router-view>
 
        </v-container>
     </v-content>
-    <v-btn
-      bottom
-      color="pink"
-      dark
-      fab
-      fixed
-      right
-      @click="dialog = !dialog"
-    >
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-    <v-dialog
-      v-model="dialog"
-      width="800px"
-    >
-      <v-card>
-        <v-card-title class="grey darken-2">
-          Create contact
-        </v-card-title>
-        <v-container>
-          <v-row>
-            <v-col
-              class="align-center justify-space-between"
-              cols="12"
-            >
-              <v-row align="center">
-                <v-avatar
-                  size="40px"
-                  class="mr-4"
-                >
-                  <img
-                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                    alt=""
-                  >
-                </v-avatar>
-                <v-text-field
-                  placeholder="Name"
-                ></v-text-field>
-              </v-row>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field
-                prepend-icon="business"
-                placeholder="Company"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field
-                placeholder="Job title"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="mail"
-                placeholder="Email"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                type="tel"
-                prepend-icon="phone"
-                placeholder="(000) 000 - 0000"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="notes"
-                placeholder="Notes"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-card-actions>
-          <v-btn
-            text
-            color="primary"
-          >More</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="dialog = false"
-          >Cancel</v-btn>
-          <v-btn
-            text
-            @click="dialog = false"
-          >Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
 
     <v-footer app>
       <!-- -->
--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,38 +1,187 @@
 <template>
   <v-row
-   align="center"
+   align="stretch"
+   align-content="stretch"
    justify="center"
+   no-gutters
+   style="max-width: 100%;"
+   class="pa-0 ma-0"
   >
-    <v-container style="max-width: 600px;">
-      <v-timeline dense clipped>
-        <v-timeline-item
-          fill-dot
-          class="white--text mb-12"
-          color="orange"
-          large
-          v-for="(sample, index) in timeline" :key="index">
-        >
-          <img :src='imagePath(sample.args.rel_dir, sample.args.image_file_name)'/>
-          <span>{{ sample.file }}</span>
-          <span>{{ sample.id }}</span>
-          <span>{{ sample.datetime }}</span>
-          <span>
-            <ul
-              v-for="(inf, index) in sample.inference_result"
-              v-bind:key="index"
-              :data-num="index + 1"
+    <v-col
+      cols="12"
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title>This is an example timeline</v-list-item-title>
+          <v-list-item-subtitle>You can setup your own Ambianic timeline.</v-list-item-subtitle>
+          <v-list-item-subtitle>Go to Settings to begin.</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-col>
+    <v-col
+      align="left"
+      justify="left"
+      cols="12"
+      class="pa-0 ma-0 fill-height"
+    >
+      <v-list
+        dense
+        class="pa-0 ma-0"
+      >
+      <v-list-item
+        three-line
+        v-for="(sample, index) in timeline" v-bind:key="index"
+        class="pa-0 ma-0"
+      >
+        <v-list-item-content>
+          <v-img
+            :src='imagePath(sample.args.rel_dir, sample.args.image_file_name)'
+            class="white--text align-end"
+            alt="Object Detection"
+            contain
+          >
+            <v-card-title>Front door person detection</v-card-title>
+            <v-container class="fill-height">
+              <v-row align="center">
+                <strong class="display-4 font-weight-regular mr-6">8</strong>
+                <v-row justify="end">
+                  <div class="headline font-weight-light">Monday</div>
+                  <div class="text-uppercase font-weight-light">February 2015</div>
+                </v-row>
+              </v-row>
+            </v-container>
+          </v-img>
+          <v-timeline
+            align-top
+            clipped
+            dense
+          >
+            <v-timeline-item
+              hide-dot
             >
-              <li>
-                <p>Category: {{inf.category}}</p>
-                <p>Confidence: {{inf.confidence}}</p>
-                <p>Box: {{inf.box}}</p>
-              </li>
-            </ul>
-          </span>
-        </v-timeline-item>
-        <infinite-loading :identifier="infiniteId" @infinite="infiniteHandler"/>
-      </v-timeline>
-    </v-container>
+              <v-card-actions>
+              <v-btn fab
+                color="success lighten-1"
+                class="mx-2"
+              >
+                <v-icon>mdi-check</v-icon>
+              </v-btn>
+              <v-btn
+                color="error"
+                fab
+                class="mx-2"
+                >
+                <v-icon>mdi-bell</v-icon>
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-pen</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+            </v-timeline-item>
+            <v-timeline-item
+                hide-dot
+            >
+                About this detection...
+                <span>{{ sample.file }}</span>
+                <span>{{ sample.id }}</span>
+                <span>{{ sample.datetime }}</span>
+                <span>
+                  <ul
+                    v-for="(inf, index) in sample.inference_result"
+                    v-bind:key="index"
+                    :data-num="index + 1"
+                  >
+                    <li>
+                      <p>Category: {{inf.category}}</p>
+                      <p>Confidence: {{inf.confidence}}</p>
+                      <p>Box: {{inf.box}}</p>
+                    </li>
+                  </ul>
+                </span>
+              </v-timeline-item>
+              <v-timeline-item
+                         color="pink"
+                         small
+              >
+                <v-row class="pt-1">
+                   <v-col cols="3">
+                     <strong>5pm</strong>
+                   </v-col>
+                   <v-col>
+                     <strong>New Icon</strong>
+                     <div class="caption">Mobile App</div>
+                   </v-col>
+                 </v-row>
+            </v-timeline-item>
+
+            <v-timeline-item
+             color="teal lighten-3"
+             small
+            >
+              <v-row class="pt-1">
+                <v-col cols="3">
+                   <strong>3-4pm</strong>
+                </v-col>
+                <v-col>
+                   <strong>Design Stand Up</strong>
+                   <div class="caption mb-2">Hangouts</div>
+                   <v-avatar>
+                     <v-img
+                       src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairFrida&accessoriesType=Kurt&hairColor=Red&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=GraphicShirt&clotheColor=Gray01&graphicType=Skull&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Disbelief&skinColor=Brown"
+                     ></v-img>
+                   </v-avatar>
+                   <v-avatar>
+
+                     <v-img
+                       src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Prescription02&hairColor=Black&facialHairType=MoustacheMagnum&facialHairColor=BrownDark&clotheType=BlazerSweater&clotheColor=Black&eyeType=Default&eyebrowType=FlatNatural&mouthType=Default&skinColor=Tanned"
+                     ></v-img>
+                   </v-avatar>
+                   <v-avatar>
+                     <v-img
+                       src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairMiaWallace&accessoriesType=Sunglasses&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Pale"
+                     ></v-img>
+                   </v-avatar>
+                </v-col>
+              </v-row>
+            </v-timeline-item>
+
+            <v-timeline-item
+             color="pink"
+             small
+            >
+             <v-row class="pt-1">
+               <v-col cols="3">
+                 <strong>12pm</strong>
+               </v-col>
+               <v-col>
+                 <strong>Lunch break</strong>
+               </v-col>
+             </v-row>
+            </v-timeline-item>
+
+            <v-timeline-item
+             color="teal lighten-3"
+             small
+            >
+             <v-row class="pt-1">
+               <v-col cols="3">
+                 <strong>9-11am</strong>
+               </v-col>
+               <v-col>
+                 <strong>Finish Home Screen</strong>
+                 <div class="caption">Web App</div>
+               </v-col>
+             </v-row>
+            </v-timeline-item>
+          </v-timeline>
+        </v-list-item-content>
+      </v-list-item>
+      </v-list>
+    </v-col>
   </v-row>
 </template>
 
@@ -40,68 +189,18 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 import axios from 'axios'
 import ambianicConf from '../../config.js'
-import InfiniteLoading from 'vue-infinite-loading'
-import Vue from 'vue'
-
-Vue.use(InfiniteLoading, { /* options */
-  slots: {
-    // keep default styles
-    noResults: 'No data available on your timeline yet.',
-    noMore: 'End of your timeline archive.'
-  }
-})
 
 const API_ROOT = ambianicConf['AMBIANIC_API_URI']
-const API_TIMELINE_PATH = API_ROOT + 'timeline'
+const API_TIMELINE_PATH = API_ROOT + 'timeline.json'
 // console.debug('API_TIMELINE_PATH: ' + API_TIMELINE_PATH)
 
 export default {
   data () {
     return {
-      timeline: [],
-      // infinite loading attributes
-      page: 1,
-      infiniteId: +new Date(),
-      addSampleForm: {
-        title: '',
-        author: '',
-        read: false
-      },
-      editSampleForm: {
-        id: '',
-        title: '',
-        author: '',
-        read: false
-      },
-      message: '',
-      showMessage: false
+      timeline: []
     }
   },
-  components: {
-    //    alert: Alert,
-  },
   methods: {
-    infiniteHandler ($state) {
-      const api = API_TIMELINE_PATH
-      axios.get(api, {
-        params: {
-          page: this.page
-        }
-      }).then(({ data }) => {
-        /* eslint no-console: "off" */
-        console.debug('data: \n' + data)
-        if (data.timeline.length) {
-          // console.debug('this.page: ' + this.page)
-          this.page += 1
-          this.timeline.push(...data.timeline)
-          // console.debug('data.timeline.length: ' + data.timeline.length)
-          // console.debug('this.timeline.length: ' + this.timeline.length)
-          $state.loaded()
-        } else {
-          $state.complete()
-        }
-      })
-    },
     imagePath (relDir, imageName) {
       let p = API_ROOT + 'data/' + relDir + '/' + imageName
       // console.debug('imagePath: ' + p)
@@ -119,105 +218,10 @@ export default {
           // eslint-disable-next-line
           console.error(error);
         })
-    },
-    addSample (payload) {
-      const path = API_TIMELINE_PATH
-      axios.post(path, payload)
-        .then(() => {
-          this.getTimeline()
-          this.message = 'Sample added!'
-          this.$refs.alert.showAlert()
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.error(error)
-          this.getTimeline()
-        })
-    },
-    updateSample (payload, sampleID) {
-      // console.debug('updating sample id: ' + sampleID + ' paylod: ' + JSON.stringify(payload));
-      const path = API_TIMELINE_PATH + `/${sampleID}`
-      axios.put(path, payload)
-        .then(() => {
-          this.getTimeline()
-          this.message = 'Sample updated!'
-          this.$refs.alert.showAlert()
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.error(error)
-          this.getTimeline()
-        })
-    },
-    initForm () {
-      this.addSampleForm.title = ''
-      this.addSampleForm.author = ''
-      this.addSampleForm.read = false
-      this.editSampleForm.id = ''
-      this.editSampleForm.title = ''
-      this.editSampleForm.author = ''
-      this.editSampleForm.read = false
-    },
-    onSubmit (evt) {
-      evt.preventDefault()
-      this.$refs.addSampleModal.hide()
-      let read = false
-      if (this.addSampleForm.read) read = true
-      const payload = {
-        title: this.addSampleForm.title,
-        author: this.addSampleForm.author,
-        read // property shorthand
-      }
-      this.addSample(payload)
-      this.initForm()
-    },
-    onReset (evt) {
-      evt.preventDefault()
-      this.$refs.addSampleModal.hide()
-      this.initForm()
-    },
-    editSample (sample) {
-      // console.info('Transfering sample from table to edit form: ' + JSON.stringify(sample));
-      this.editSampleForm = sample
-    },
-    onSubmitUpdate (evt) {
-      evt.preventDefault()
-      this.$refs.editSampleModal.hide()
-      let read = false
-      if (this.editSampleForm.read) read = true
-      const payload = {
-        title: this.editSampleForm.title,
-        author: this.editSampleForm.author,
-        read
-      }
-      this.updateSample(payload, this.editSampleForm.id)
-    },
-    onResetUpdate (evt) {
-      evt.preventDefault()
-      this.$refs.editSampleModal.hide()
-      this.initForm()
-      this.getTimeline() // update view
-    },
-    deleteSample (sampleID) {
-      const path = API_TIMELINE_PATH + `/${sampleID}`
-      axios.delete(path)
-        .then(() => {
-          this.getTimeline()
-          this.message = 'Sample removed!'
-          this.$refs.alert.showAlert()
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.error(error)
-          this.getTimeline()
-        })
-    },
-    onDeleteSample (sample) {
-      this.deleteSample(sample.id)
     }
   },
   created () {
-    // this.getTimeline();
+    this.getTimeline()
   }
 }
 </script>
