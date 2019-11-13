fbc0ec6ea9351121aad14fd0527693c5847162b0
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-11-13 11:33:52 -0600

    feat(pwa): offline availability

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,13 @@ module.exports = {
     'themeColor': '#179aff',
     'msTileColor': '#179aff',
     'appleMobileWebAppCapable': 'yes',
+    'iconPaths': {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
     'workboxPluginMode': 'GenerateSW',
     'workboxOptions': {
       'runtimeCaching': [
