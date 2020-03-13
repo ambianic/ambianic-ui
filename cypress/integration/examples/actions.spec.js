e4b48c8cb90f04762d6867ff4763aaa466325db3
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2020-03-13 15:46:24 -0500

    test: add Cypress end to end testing to CI

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -2,7 +2,7 @@
 
 context('Actions', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/actions')
+    cy.visit('http://localhost:8080')
   })
 
   // https://on.cypress.io/interacting-with-elements
