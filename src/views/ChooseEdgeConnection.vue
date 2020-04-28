4e2d8b3c6643a80d3f6b21ac7dd3e2b6a77c6f67
Björn Kristensson Alfsson <bjorn.alfsson@gmail.com>
2020-04-28 13:45:16 +0200

    Added watcher for validation
    Button to connect to remote network is
    not visible until a valid IP number is written.
    
    It checks that there is three dots and numbers between 1 and 255
    after each dot

--- src/views/ChooseEdgeConnection.vue
+++ src/views/ChooseEdgeConnection.vue
@@ -1,46 +1,34 @@
 <template>
   <app-frame>
-    <v-row
-      align="start"
-      justify="space-around"
-      class="container"
-    >
+    <v-row align="start" justify="space-around" class="container">
       <div class="local">
-        <p class="text">
-          Connect to local network
-        </p>
+        <p class="text">Connect to local network</p>
         <router-link :to="{name: 'edge-connect', params: {edgeAddress: 'localhost'}}">
-          <v-btn class="localButton">
-            Local Network
-          </v-btn>
+          <v-btn class="localButton">Local Network</v-btn>
         </router-link>
       </div>
       <div class="middleLine">OR</div>
       <div class="remote">
-          <form class="text">
-        Connect to remote network
-        <h6 class="fineDetails">(must enter room ID to Ambianic network)</h6>
-        <p></p>
-        <label for="ambianicIPAddress" class="fineDetails">Room ID to Ambianic Network</label>
-        <p>
-        <input
-          type="text"
-          id="ambianicIPAddress"
-          class="inputbox"
-          placeholder="Enter Room ID"
-          v-model="ipAddress"
-        >
-        </p>
-        <p>
-        <router-link :to="{name: 'edge-connect', params: {edgeAddress: ipAddress}}">
-          <v-btn
-            :disabled="!ipAddress"
-          >
-            REMOTE NETWORK
-          </v-btn>
-        </router-link>
-        </p>
-      </form>
+        <form class="text">
+          Connect to remote network
+          <h6 class="fineDetails">(must enter IP to Ambianic network)</h6>
+          <p></p>
+          <label for="ambianicIPAddress" class="fineDetails">IP to Ambianic Network*</label>
+          <p>
+            <input
+              type="text"
+              id="ambianicIPAddress"
+              class="inputbox"
+              placeholder="Enter IP"
+              v-model="ipAddress"
+            />
+          </p>
+          <p>
+            <router-link v-if="correctIP" :to="{name: 'edge-connect', params: {edgeAddress: ipAddress}}">
+              <v-btn>REMOTE NETWORK</v-btn>
+            </router-link>
+          </p>
+        </form>
       </div>
     </v-row>
   </app-frame>
@@ -50,7 +38,8 @@ import AppFrame from '@/components/AppFrame.vue'
 export default {
   data: () => {
     return {
-      ipAddress: ''
+      ipAddress: '',
+      correctIP: false
       // connectionStatus: '',
       // connectionTip: '',
       // testInProgress: false,
@@ -61,9 +50,23 @@ export default {
   },
   computed: {},
   methods: {
+    validateIP (value) {
+      if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(value)) {
+        this.correctIP = true
+        return this.correctIP
+      } else {
+        console.log('Error')
+      }
+    }
   },
   components: {
     AppFrame
+  },
+  watch: {
+    ipAddress (value) {
+      this.ipAddress = value
+      this.validateIP(value)
+    }
   }
 }
 </script>
@@ -74,6 +77,14 @@ export default {
   flex-direction: row;
   margin: 0 auto;
 }
+
+#ambianicIPAddress {
+  border: 2px ridge lightgray;
+  padding: 6px;
+  border-radius: 5px;
+  width: 81%;
+}
+
 hr {
   margin: 1rem 0;
 }
