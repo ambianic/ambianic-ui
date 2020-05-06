867356d450f6de013b3a1154d463a6bbd6fe8ed2
Nasr Galal <nasrbue@gmail.com>
2020-05-06 03:00:47 +0200

    feat: create reusable components for code dryness

---
+++ src/components/shared/ListItem.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <v-list-item :two-line="twoLine">
+    <v-list-item-icon>
+      <v-icon>
+        mdi-{{ iconName }}
+      </v-icon>
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title class="headline">
+        {{ title }}
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        {{ subtitle }}
+      </v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    align: {
+      type: String,
+      default: null
+    },
+    justify: {
+      type: String,
+      default: null
+    },
+    title: {
+      type: String,
+      default: null
+    },
+    subtitle: {
+      type: String,
+      default: null
+    },
+    iconName: {
+      type: String,
+      default: null
+    },
+    twoLine: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
