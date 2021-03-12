86cb15e20cb9fd581bf756211c2c8cbb9de0a044
victory <vickywane@gmail.com>
2021-03-12 21:06:11 +0100

    feat: fix ci break

--- src/components/shared/ListItem.vue
+++ src/components/shared/ListItem.vue
@@ -48,7 +48,7 @@
     </v-list-item-content>
 
     <v-list-item-content v-else>
-      <v-list-item-title class="headline">
+      <v-list-item-title data-cy="title-text" class="headline">
         {{ title }}
       </v-list-item-title>
 
