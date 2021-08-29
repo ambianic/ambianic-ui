3697b41491ed9881cbcfcb1824a6dd0cfc595448
Ivelin Ivanov <ivelin@ambianic.ai>
2021-08-29 10:35:38 -0500

    fix: install wizard bugs (#707)

--- cypress/integration/ambianic-tests/timeline.spec.js
+++ cypress/integration/ambianic-tests/timeline.spec.js
@@ -3,12 +3,12 @@
 context('Timeline', () => {
   before(() => {
     cy.visit('http://localhost:8080')
-  //   cy.get('[data-cy=timeline]').click()
-    cy.visit("./timeline")
+    //   cy.get('[data-cy=timeline]').click()
+    cy.visit('./timeline')
   })
 
   it('Should browse to timeline', () => {
-      cy.url().should('include', '/timeline')
+    cy.url().should('include', '/timeline')
   })
 
   // Try to get this to work once we have mock data
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,6 +9,9 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
+const { startDevServer } = require('@cypress/webpack-dev-server')
+const webpackConfig = require('@vue/cli-service/webpack.config.js')
+
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
@@ -20,6 +23,15 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
   require('@cypress/code-coverage/task')(on, config)
   on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+
+  // conigure vue component testing 
+  // ref: https://www.cypress.io/blog/2021/04/06/getting-start-with-cypress-component-testing-vue-2-3/
+  on('dev-server:start', options =>
+    startDevServer({
+      options,
+      webpackConfig
+    }))
+
   // IMPORTANT to return the config object
   // with any changed environment variables
   return config
